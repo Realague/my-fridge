dae0645c8fccc9a27d607505c55de18cdcb617d8
Realague <realague@gmail.com>
2025-12-28 13:01:41 +0100

    pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -23,6 +24,30 @@ export default defineConfig(({ mode }) => {
       react(),
       mode === 'development' &&
       componentTagger(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico'],
+        manifest: {
+          name: 'MyFridge',
+          short_name: 'MyFridge',
+          start_url: '.',
+          display: 'standalone',
+          background_color: '#ffffff',
+          theme_color: '#000000',
+          icons: [
+            {
+              src: 'favicon.ico',
+              sizes: '192x192',
+              type: 'image/ico'
+            },
+            {
+              src: 'favicon.ico',
+              sizes: '512x512',
+              type: 'image/ico'
+            }
+          ]
+        }
+      })
     ].filter(Boolean),
     resolve: {
       alias: {

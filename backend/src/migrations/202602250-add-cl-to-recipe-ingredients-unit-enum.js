358fa4de767a2a7af043657771ed99fde44c5c55
Realague <realague@gmail.com>
2026-02-26 10:43:15 +0100

    fix crash

--- backend/src/migrations/202602250-add-cl-to-recipe-ingredients-unit-enum.js
+++ backend/src/migrations/202602250-add-cl-to-recipe-ingredients-unit-enum.js
@@ -2,16 +2,12 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  // Disable transaction for this migration because ALTER TYPE ... ADD VALUE
-  // cannot run inside a transaction in PostgreSQL
+  // ALTER TYPE ... ADD VALUE cannot run inside a transaction in PostgreSQL
   transaction: false,
-  
+
   async up(queryInterface) {
     await queryInterface.sequelize.query(`
-      ALTER TYPE "enum_items_defaultUnit" ADD VALUE IF NOT EXISTS 'cl';
-    `);
-    await queryInterface.sequelize.query(`
-      ALTER TYPE "enum_stored_items_unit" ADD VALUE IF NOT EXISTS 'cl';
+      ALTER TYPE "enum_recipe_ingredients_unit" ADD VALUE IF NOT EXISTS 'cl';
     `);
   },
 
